cbf5036f05c811609f901d695b289391f2922ebd
Gvozdoslav <remrostrus2123@yandex.ru>
2022-12-11 15:30:04 +0300

    Initial commit

---
+++ src/main/java/com/petdoctor/EnrollmentServiceApp.java
@@ -0,0 +1,11 @@
+package com.petdoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EnrollmentServiceApp.class, args);
+    }
+}
